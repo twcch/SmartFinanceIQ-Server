cc131af08b6e6f3ac2dc8c56b776694fbb32eaf4
謝志謙 <twcch1218@gmail.com>
2024-04-26 20:53:42 +0800

    初始化

---
+++ src/main/java/com/smartfinanceiq/SmartFinanceIqServerApplication.java
@@ -0,0 +1,13 @@
+package com.smartfinanceiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinanceIqServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFinanceIqServerApplication.class, args);
+    }
+
+}
